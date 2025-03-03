719e9cd124d409d14bbf76bff4708ac52b0ffd05
sharaf911 <ouertatenicharaf@isikef.u-jendouba.tn>
2025-03-03 13:27:08 +0100

    second commit

--- src/app/features/orders/order-details/order-details.component.ts
+++ src/app/features/orders/order-details/order-details.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../../shared/components/navbar/navbar.component";
+import { SidebarComponent } from "../../../shared/components/sidebar/sidebar.component";
 
 @Component({
     selector: 'app-order-details',
-    imports: [],
+    imports: [NavbarComponent, SidebarComponent],
     templateUrl: './order-details.component.html',
     styleUrl: './order-details.component.css'
 })
